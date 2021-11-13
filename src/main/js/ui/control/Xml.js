a088972bb72a523aad29593d83e7f6a317bd60a0
aalplotnikov <aalplotnikov@gmail.com>
2021-11-13 22:34:45 +0400

    xml

--- src/main/js/ui/control/Xml.js
+++ src/main/js/ui/control/Xml.js
@@ -4,20 +4,58 @@
 Z8.define('org.zenframework.z8.template.controls.Xml', {
 	// Имя наследуемого класса
 	extend: 'Z8.form.field.TextArea',
+	tag: 'div',
+	id: 'editor',
+	editor: '',
+	
+	initComponent: function() {
+		Z8.form.field.TextArea.prototype.initComponent.call(this);
+	},
+	
+	completeRender: function() {
+		Z8.form.field.TextArea.prototype.completeRender.call(this);
 
-	// Переопределение метода validate(), который вызывается при каждом изменении значения поля
-	onInput: function() {
-		var el = document.querySelector("#" + this.getId());
-		var str = this.getValue();
-		for(var i = 0; i < str.length; i++) {
-			if (str[i] === '<')
-				str[i] = '&lt;';
-			if (str[i] === '>')
-				str[i] = '&gt;';
+    	this.editor = ace.edit("editor");
+		this.editor.session.setMode("ace/mode/xml");
+		
+	},
+	
+	onFocusOut: function(event, target) {
+		var dom = DOM.get(this);
+		target = event.relatedTarget;
+
+		if(dom == target || DOM.isParentOf(dom, target))
+			return false;
+
+		if(this.autoSave && !this.instantAutoSave) {
+			if(this.isValid()) {
+				this.suspendCheckChange--;
+				this.setValue(this.editor.getValue());
+				this.suspendCheckChange++;
+			} else
+				this.initValue(this.originalValue, this.originalDisplayValue);
 		}
-		this.setValue(str);
-		hljs.highlightElement(el, {language: 'xml'});
-		console.log(this.dom);
-	}
 
+		DOM.removeCls(this, 'focus');
+		this.fireEvent('focusOut', this);
+		return true;
+	},
+	
+	initValue: function(value, displayValue) {
+		value = value !== undefined ?  value : this.value;
+
+		this.initializing = true;
+		this.suspendCheckChange++;
+		this.setValue(value, displayValue);
+		this.suspendCheckChange--;
+		this.initializing = false;
+
+		this.initialValue = this.originalValue = this.lastValue = this.getValue();
+		this.originalDisplayValue = displayValue;
+		if(this.editor.setValue !== undefined)
+			this.editor.setValue(value);
+	},
 });
+
+
+
